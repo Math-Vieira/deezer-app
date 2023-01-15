81f5bb48a049cca48cfc2c92f727af5c29da3baf
math-vieira <matheusvieira9153@gmail.com>
2023-01-15 18:32:38 -0300

    feat: add loading screen component

---
+++ src/helper/components/LoadingScreen/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as S from './style'
+
+const LoadingScreen = (): JSX.Element => {
+  return <S.Container>LoadingScreen</S.Container>
+}
+
+export default LoadingScreen
