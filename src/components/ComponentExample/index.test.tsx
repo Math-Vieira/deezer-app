f79646122a01a429b7dcc220b5058ef07f8398da
math-vieira <matheusvieira9153@gmail.com>
2023-01-15 18:11:36 -0300

    feat:  add generic component example

---
+++ src/components/ComponentExample/index.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+describe('ComponentExample', () => {
+  test('should first', () => {})
+
+  test('should second', () => {})
+
+  test('should third', () => {})
+})
---
+++ src/components/ComponentExample/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import * as S from './style'
+import { Props } from './model'
+
+const ComponentExample = (props: Props): JSX.Element => {
+  return <S.Container>ComponentExample</S.Container>
+}
+
+export default ComponentExample
