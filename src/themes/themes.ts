fdd7361d79b0bf116719f58455ce776c7084b905
math-vieira <matheusvieira9153@gmail.com>
2023-01-16 00:02:41 -0300

    refactor: add header properties

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -2,6 +2,8 @@ export interface ThemeContract {
   mainColor: string
   secondaryColor: string
   tertiaryColor: string
+  headerBackground: string
+  headerBoxShadow: string
   textColor: {
     primaryColor: string
     secondaryColor: string
@@ -12,6 +14,8 @@ export const darkTheme: ThemeContract = {
   mainColor: '#0B0B0B',
   secondaryColor: '#272727',
   tertiaryColor: '#747474',
+  headerBackground: 'rgba(255, 255, 255, 0.11)',
+  headerBoxShadow: '0px 2px 2px rgba(255, 255, 255, 0.25)',
   textColor: {
     primaryColor: '#FFFFFF',
     secondaryColor: '#929292'
