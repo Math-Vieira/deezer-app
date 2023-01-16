936b14849a104d3ed00eb216d8a606ed28f054be
math-vieira <matheusvieira9153@gmail.com>
2023-01-16 00:59:54 -0300

    style: add home main style

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    background-color: red;
+export const Container = styled.section`
+  background-color: ${({ theme }) => theme.mainColor};
+  min-height: 100vh;
 `
