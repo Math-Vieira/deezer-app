b81cf03177b4a858038ed6eaa2ea341fd5d7f1d9
math-vieira <matheusvieira9153@gmail.com>
2023-01-15 22:43:25 -0300

    refactor: add first theme properties and interface

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -1,2 +1,20 @@
-export const lightTheme = {}
-export const darkTheme = {}
+export interface ThemeContract {
+  mainColor: string
+  secondaryColor: string
+  tertiaryColor: string
+  textColor: {
+    primaryColor: string
+    secondaryColor: string
+  }
+}
+
+export const darkTheme: ThemeContract = {
+  mainColor: '#0B0B0B',
+  secondaryColor: '#272727',
+  tertiaryColor: '#747474',
+  textColor: {
+    primaryColor: '#FFFFFF',
+    secondaryColor: '#929292'
+  }
+}
+// export const lightTheme = {}
