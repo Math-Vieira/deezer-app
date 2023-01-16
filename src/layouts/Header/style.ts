cb14955f87a5533833252895ba94527bd9b56318
math-vieira <matheusvieira9153@gmail.com>
2023-01-16 00:07:36 -0300

    feat: create header first style version

--- src/layouts/Header/style.ts
+++ src/layouts/Header/style.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    background-color: red;
+  background-color: ${({ theme }) => theme.headerBackground};
+  backdrop-filter: blur(3px);
+  color: red;
+  position: fixed;
+  top: 0;
+  padding: 1rem 0;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  box-shadow: ${({ theme }) => theme.headerBoxShadow};
 `
