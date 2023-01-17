6f5a1de12447959c7f7e5e8d2251c8c1b85e324f
math-vieira <matheusvieira9153@gmail.com>
2023-01-16 23:47:17 -0300

    style: create header styled components

--- src/layouts/Header/style.ts
+++ src/layouts/Header/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Header = styled.header`
   background-color: ${({ theme }) => theme.headerBackground};
   backdrop-filter: blur(3px);
   color: red;
@@ -11,4 +11,33 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   box-shadow: ${({ theme }) => theme.headerBoxShadow};
+`
+export const HeaderContentContainer = styled.div`
+  min-width: 90%;
+  padding: 0 1rem;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Nav = styled.nav`
+  & u {
+    display: flex;
+    gap: 1.5rem;
+  }
+
+  & u li a {
+    color: ${({ theme }) => theme.textColor.secondaryColor};
+    font-size: 1.1rem;
+    font-family: 'Saira', sans-serif;
+  }
+
+  & u li a:hover {
+    color: ${({ theme }) => theme.textColor.primaryColor};
+  }
+
+  & u li .active {
+    color: ${({ theme }) => theme.textColor.primaryColor};
+  }
 `
