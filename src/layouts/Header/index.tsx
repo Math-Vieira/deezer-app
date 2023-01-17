e942512d28b07baac11e5b69d7bca7bbbc0a50cc
math-vieira <matheusvieira9153@gmail.com>
2023-01-16 23:55:52 -0300

    feat: create header shape

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import LightMode from '../../assets/svg-components/LightMode'
+import LogoSvg from '../../assets/svg-components/Logo'
 import * as S from './style'
 
 const Header = (): JSX.Element => {
-  return <S.Container>Header</S.Container>
+  return (
+    <S.Header>
+      <S.HeaderContentContainer>
+        <a href="/">
+          <LogoSvg />
+        </a>
+        <S.Nav>
+          <u>
+            <li>
+              <NavLink className="link" to="/">
+                Início
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="link" to="/favorites">
+                Favoritos
+              </NavLink>
+            </li>
+            <li>
+              <LightMode />
+            </li>
+          </u>
+        </S.Nav>
+      </S.HeaderContentContainer>
+    </S.Header>
+  )
 }
 
 export default Header
