82dc29a5b0dd406515f038dfee78f9e2f5e565ea
math-vieira <matheusvieira9153@gmail.com>
2023-01-18 15:43:09 -0300

    feat: add h2 svg icon

---
+++ src/assets/svg-components/H2/index.tsx
@@ -0,0 +1,19 @@
+import * as S from './style'
+
+const H2 = (): JSX.Element => {
+  return (
+    <S.Svg
+      width="54"
+      height="23"
+      viewBox="0 0 54 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="6" y="9" width="48" height="5" rx="2.5" fill="#D9D9D9" />
+      <rect width="48" height="5" rx="2.5" fill="#D9D9D9" />
+      <rect y="18" width="48" height="5" rx="2.5" fill="#D9D9D9" />
+    </S.Svg>
+  )
+}
+
+export default H2
