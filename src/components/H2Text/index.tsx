1c240bf6ec871dabbad7e04919cb0ea94940931b
math-vieira <matheusvieira9153@gmail.com>
2023-01-18 16:05:34 -0300

    refactor: rename component export

--- src/components/H2Text/index.tsx
+++ src/components/H2Text/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import H2 from '../../assets/svg-components/H2'
 import * as S from './style'
 
-const index = ({ children }: { children: React.ReactNode }): JSX.Element => {
+const H2Text = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
     <S.H2>
-      <H2 /> {children}
+      <H2 /> <span>{children}</span>
     </S.H2>
   )
 }
 
-export default index
+export default H2Text
