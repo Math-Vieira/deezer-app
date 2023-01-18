0e5e6ed6c1537a97004a82237573892ca9ba6be5
math-vieira <matheusvieira9153@gmail.com>
2023-01-18 16:03:23 -0300

    style: align h2 text

--- src/components/H2Text/style.ts
+++ src/components/H2Text/style.ts
@@ -2,4 +2,7 @@ import styled from 'styled-components'
 
 export const H2 = styled.h2`
   color: ${({ theme }) => theme.textColor.primaryColor};
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 `
