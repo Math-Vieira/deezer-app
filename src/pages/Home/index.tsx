549e66156a04b43b822300ebdabd0471c69947db
math-vieira <matheusvieira9153@gmail.com>
2023-01-18 23:29:44 -0300

    feat: add home search input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
+import H2Text from '../../components/H2Text'
+import Input from '../../components/Input'
 import * as S from './style'
 
 const Home = (): JSX.Element => {
-  return <S.Container>Home</S.Container>
+  const [searchText, setSearchText] = useState<string>('')
+
+  return (
+    <S.Container>
+      <section className="centralizer">
+        <S.SearchContainer>
+          <H2Text>Pesquise por uma música</H2Text>
+          <Input
+            placeholder='Pesquise uma música...'
+            id="search-song"
+            type="text"
+            value={searchText}
+            setValue={setSearchText}
+          />
+        </S.SearchContainer>
+      </section>
+    </S.Container>
+  )
 }
 
 export default Home
