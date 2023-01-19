9063c2338d779181161d0f4f0fef27b86a1b138d
math-vieira <matheusvieira9153@gmail.com>
2023-01-18 23:27:28 -0300

    style: create searchcontainer styled component

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 
-export const Container = styled.section`
+export const Container = styled.main`
   background-color: ${({ theme }) => theme.mainColor};
   min-height: 100vh;
+  padding-top: 10rem;
+`
+
+export const SearchContainer = styled.div`
+  width: fit-content;
 `
