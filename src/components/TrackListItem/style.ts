555cd1fbaa61101f9b407ed16c2679ec8831dbe2
math-vieira <matheusvieira9153@gmail.com>
2023-01-21 20:02:52 -0300

    style: create styled components to trackListItem

---
+++ src/components/TrackListItem/style.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+export const Li = styled.li`
+  position: relative;
+  background-color: ${({ theme }) => theme.secondaryColor};
+  margin-right: 10px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  width: auto;
+  padding: 10px;
+  & div img {
+    height: 40px;
+  }
+`
+
+export const ContentPart = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`
+
+export const TrackInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: -2px;
+`
+
+export const TrackTitle = styled.div`
+  color: ${({ theme }) => theme.textColor.primaryColor};
+  max-height: 25px;
+`
+
+export const TrackArtist = styled.div`
+  color: ${({ theme }) => theme.textColor.secondaryColor};
+  margin-left: 4px;
+  max-height: 25px;
+`
+
+export const Duration = styled.div`
+  color: ${({ theme }) => theme.textColor.primaryColor};
+  position: absolute;
+  right: 5px;
+  top: 1px;
+  font-size: 10px;
+`
