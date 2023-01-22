9a916e59953b8bf0a53572c9b9190b6dce3bceab
math-vieira <matheusvieira9153@gmail.com>
2023-01-21 22:16:08 -0300

    feat: create action buttons component

---
+++ src/components/ActionButtons/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './style'
+
+const ActionButtons = (): JSX.Element => {
+  return (
+    <S.Container>
+      <S.ActionButton>a</S.ActionButton>
+      <S.ActionButton>b</S.ActionButton>
+      <S.ActionButton>c</S.ActionButton>
+    </S.Container>
+  )
+}
+
+export default ActionButtons
