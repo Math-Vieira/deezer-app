293d2b222205c943b9e8f45bc043e090e6139f60
math-vieira <matheusvieira9153@gmail.com>
2023-01-22 03:19:30 -0300

    feat: add favorite and link features

--- src/components/ActionButtons/index.tsx
+++ src/components/ActionButtons/index.tsx
@@ -1,11 +1,48 @@
+import Deezer from '../../assets/svg-components/Deezer'
+import Favorite from '../../assets/svg-components/Favorite'
+import Play from '../../assets/svg-components/Play'
+import { Props } from './model'
 import * as S from './style'
+import * as F from './functions'
+import { useContext, useEffect, useState } from 'react'
+import { GlobalContext } from '../../context/GlobalContext'
+import TrashCan from '../../assets/svg-components/TrashCan'
 
-const ActionButtons = (): JSX.Element => {
+const ActionButtons = ({ link, fullTrackInfo }: Props): JSX.Element => {
+  const { favoriteTracks, setFavoriteTracks } = useContext(GlobalContext)
+  const [isFavorite, setIsFavorite] = useState<boolean>(false)
+  const trackId = fullTrackInfo.id
+
+  useEffect(() => {
+    const findFavorite = favoriteTracks.filter((e) => e.id === fullTrackInfo.id)
+    if (findFavorite.length) setIsFavorite(true)
+  }, [])
   return (
     <S.Container>
-      <S.ActionButton>a</S.ActionButton>
-      <S.ActionButton>b</S.ActionButton>
-      <S.ActionButton>c</S.ActionButton>
+      <S.ActionButton>
+        <Play />
+      </S.ActionButton>
+      <S.ActionButton
+        title={isFavorite ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}
+        onClick={() =>
+          F.favoriteToggle({
+            isFavorite,
+            setIsFavorite,
+            setFavoriteTracks,
+            trackId,
+            fullTrackInfo
+          })
+        }
+      >
+        {!isFavorite && <Favorite />}
+        {isFavorite && <TrashCan />}
+      </S.ActionButton>
+      <S.ActionButton
+        title="Ouvir completo"
+        onClick={() => F.openTrackLink(link)}
+      >
+        <Deezer />
+      </S.ActionButton>
     </S.Container>
   )
 }
