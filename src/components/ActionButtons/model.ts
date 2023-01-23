37214379287f9fb65fe125bcf594ce69ea1d7fa9
math-vieira <matheusvieira9153@gmail.com>
2023-01-23 02:55:47 -0300

    feat: add keys to model

--- src/components/ActionButtons/model.ts
+++ src/components/ActionButtons/model.ts
@@ -1,4 +1,9 @@
+import { AudioController } from '../TracksContainer/model'
 export interface Props {
   link: string
   fullTrackInfo: any
+  audioController: (object: AudioController) => void
+  currentTrack: string
+  playing: boolean
+  setPlaying: React.Dispatch<React.SetStateAction<boolean>>
 }
