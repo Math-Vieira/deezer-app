f6ad62a70017cba2b577a310c40a05fb0b124c3a
math-vieira <matheusvieira9153@gmail.com>
2023-01-23 06:16:06 -0300

    refactor: refactor get functions

--- src/pages/Home/functions.ts
+++ src/pages/Home/functions.ts
@@ -1,19 +1,68 @@
 import { UseAxios } from '../../hooks/useAxios'
 
+let wait = false
+let pageNumber = 0
+
 export const getChartSongs = async (
   { request }: UseAxios,
-  pageNumber: number
+  ulRef: React.RefObject<HTMLUListElement>,
+  isInitial: boolean
 ): Promise<void> => {
-  await request({ url: `/chart/tracks/10/${pageNumber * 10}`, method: 'GET' })
+  if (isInitial) {
+    await request({ url: '/chart/tracks/10/0', method: 'GET' })
+    return
+  }
+
+  if (ulRef.current) {
+    const ul = ulRef.current
+    const scroll = ul.scrollTop
+    const height = ul.scrollHeight - ul.clientHeight
+
+    if (scroll > height * 0.9 && !wait) {
+      wait = true
+      pageNumber += 1
+      await request({
+        url: `/chart/tracks/10/${pageNumber * 10}`,
+        method: 'GET'
+      })
+      setTimeout(() => {
+        wait = false
+      }, 500)
+    }
+  }
 }
 
 export const searchSong = async (
   { request }: UseAxios,
-  pageNumber: number,
+  ulRef: React.RefObject<HTMLUListElement>,
+  isInitial: boolean,
   searchText: string
 ): Promise<void> => {
-  await request({
-    url: `/search/${searchText}/tracks/10/${pageNumber * 10}`,
-    method: 'GET'
-  })
+  if (isInitial) {
+    await request({ url: `/search/${searchText}/tracks/10/0`, method: 'GET' })
+    return
+  }
+
+  if (ulRef.current) {
+    const ul = ulRef.current
+    const scroll = ul.scrollTop
+    const height = ul.scrollHeight - ul.clientHeight
+
+    if (scroll > height * 0.9 && !wait) {
+      wait = true
+      pageNumber += 1
+      console.log('entrei aqui 2')
+      await request({
+        url: `/search/${searchText}/tracks/10/${pageNumber * 10}`,
+        method: 'GET'
+      })
+      setTimeout(() => {
+        wait = false
+      }, 500)
+    }
+  }
+}
+
+export const resetPageNumber = (): void => {
+  pageNumber = 0
 }
