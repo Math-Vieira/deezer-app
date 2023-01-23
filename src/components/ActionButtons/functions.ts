837783897703c702d1d30aa865a33208c9f0c764
math-vieira <matheusvieira9153@gmail.com>
2023-01-23 02:57:13 -0300

    feat: create playToggle function

--- src/components/ActionButtons/functions.ts
+++ src/components/ActionButtons/functions.ts
@@ -1,9 +1,19 @@
+import { AudioController } from '../TracksContainer/model'
+
 interface ChangeFavoriteState {
   isFavorite: boolean
   setIsFavorite: React.Dispatch<React.SetStateAction<boolean>>
   setFavoriteTracks: React.Dispatch<React.SetStateAction<any[]>> | undefined
   trackId: number
   fullTrackInfo: any
+}
+
+interface PlayToggle {
+  currentTrack: string
+  previewTrack: any
+  playing: boolean
+  setPlaying: React.Dispatch<React.SetStateAction<boolean>>
+  audioController: (object: AudioController) => void
 }
 
 export const openTrackLink = (url: string): void => {
@@ -26,5 +36,36 @@ export const favoriteToggle = ({
   if (!isFavorite && setFavoriteTracks) {
     setFavoriteTracks((prev) => [...prev, fullTrackInfo])
     setIsFavorite(true)
+  }
+}
+
+export const playToggle = ({
+  currentTrack,
+  previewTrack,
+  playing,
+  setPlaying,
+  audioController
+}: PlayToggle): void => {
+  if (currentTrack === previewTrack && !playing) {
+    audioController({ previewTrack, command: 'PLAY' })
+    setPlaying(true)
+    return
+  }
+
+  if (currentTrack === previewTrack && playing) {
+    audioController({ previewTrack, command: 'PAUSE' })
+    setPlaying(false)
+    return
+  }
+
+  if (currentTrack !== previewTrack && !playing) {
+    audioController({ previewTrack, command: 'PLAY' })
+    setPlaying(true)
+    return
+  }
+
+  if (currentTrack !== previewTrack && playing) {
+    audioController({ previewTrack, command: 'PLAY' })
+    setPlaying(true)
   }
 }
