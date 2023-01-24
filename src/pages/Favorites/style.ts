73d6a25ea1b2db0a12360e3c6523f67991e39df2
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 05:01:38 -0300

    style: create Tracks container

--- src/pages/Favorites/style.ts
+++ src/pages/Favorites/style.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    background-color: red;
+export const Container = styled.main`
+  background-color: ${({ theme }) => theme.mainColor};
+  min-height: 100vh;
+  padding-top: 10rem;
+  padding-bottom: 2rem;
+`
+
+export const SearchContainer = styled.div`
+  width: fit-content;
+`
+
+export const P = styled.p`
+  margin-top: 10px;
+  color: ${({ theme }) => theme.textColor.secondaryColor};
+`
+
+export const Tracks = styled.div`
+  display: flex;
+  justify-content: space-between;
 `
