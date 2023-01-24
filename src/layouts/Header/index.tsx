97bd42646eb1068012fe12855fe35792776cc3f0
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 18:23:17 -0300

    feat: implement i18n and useToggle

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -8,22 +8,35 @@ import useMediaQuery from '../../hooks/useMediaQuery'
 import MobileMenu from '../../components/MobileMenu'
 import { GlobalContext } from '../../context/GlobalContext'
 import DarkMode from '../../assets/svg-components/DarkMode'
+import { useTranslation } from 'react-i18next'
+import { useToggle } from '../../hooks/useToggle'
 
 const Header = (): JSX.Element => {
-  const [mobileMenu, setMobileMenu] = useState<boolean>(true)
+  const { state: mobileMenu, toggle: toggleMenu } = useToggle(true)
+  const [language, setLanguage] = useState<string>('en-US')
   const { toggleTheme, themeId } = useContext(GlobalContext)
   const isMobile = useMediaQuery('(max-width: 980px)')
-  const toggleMobile = (): void => {
-    setMobileMenu((prev) => !prev)
+  const { t, i18n } = useTranslation()
+
+  const toggleLanguage = (): void => {
+    if (language === 'en-US') {
+      setLanguage('pt-BR')
+    } else {
+      setLanguage('en-US')
+    }
   }
+
   useEffect(() => {
-    setMobileMenu(isMobile)
-  }, [isMobile])
+    void i18n.changeLanguage(language)
+  }, [language])
 
   return (
     <>
       {isMobile && (
-        <MobileMenu mobileMenu={mobileMenu} toggleMobile={toggleMobile} />
+        <MobileMenu
+          mobileMenu={mobileMenu}
+          toggleMobile={toggleMenu}
+        />
       )}
 
       <S.Header mobileMenu={mobileMenu && isMobile}>
@@ -33,22 +46,24 @@ const Header = (): JSX.Element => {
           </a>
           <S.Nav>
             <u>
-              <li onClick={toggleMobile}>
+              <li onClick={toggleMenu}>
                 <NavLink className="link" to="/">
-                  Início
+                  {t('Home:nav.home')}
                 </NavLink>
               </li>
-              <li onClick={toggleMobile}>
+              <li onClick={toggleMenu}>
                 <NavLink className="link" to="/favorites">
-                  Favoritos
+                  {t('Home:nav.favorites')}
                 </NavLink>
               </li>
-              <li onClick={toggleMobile}>
+              <li onClick={toggleMenu}>
                 <span onClick={toggleTheme}>
                   {themeId === 1 && <LightMode />}
                   {themeId === 2 && <DarkMode />}
                 </span>
-                <Language />
+                <span onClick={toggleLanguage}>
+                  <Language />
+                </span>
               </li>
             </u>
           </S.Nav>
