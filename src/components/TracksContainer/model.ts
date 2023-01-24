99892d2f4c6176d4dbf159167af893f43bd698c6
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 02:30:55 -0300

    feat: implement setCurrentTrack

--- src/components/TracksContainer/model.ts
+++ src/components/TracksContainer/model.ts
@@ -1,8 +1,10 @@
+import { CurrentTrack } from '../../pages/Home/model'
 export interface Props {
   tracksList: any[]
   loading?: boolean | null
   error?: any
   ulRef?: React.RefObject<HTMLUListElement>
+  setCurrentTrackInfo: React.Dispatch<React.SetStateAction<CurrentTrack | null>>
 }
 
 export interface AudioController {
