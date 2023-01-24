45e3c4d252fac67786d60a976e0d7c074f687cc5
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 02:25:58 -0300

    feat: create CardTrackInfo component

---
+++ src/components/CardTrackInfo/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as S from './style'
+import { Props } from './model'
+
+const CardTrackInfo = ({ background, title, artist }: Props): JSX.Element => {
+  return (
+    <S.Container>
+      <S.TextContainer justify="start">
+        {title ? title.toUpperCase() : '...'}
+      </S.TextContainer>
+      <S.CardInfo background={background} />
+      <S.TextContainer justify="end">
+        {artist ? artist.toUpperCase() : '...'}
+      </S.TextContainer>
+    </S.Container>
+  )
+}
+
+export default CardTrackInfo
