90ee0b4c869c1c21d3fa1296f58e3fd20846a164
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 14:10:55 -0300

    style: add animeLeft

--- src/assets/svg-components/LightMode/index.tsx
+++ src/assets/svg-components/LightMode/index.tsx
@@ -8,6 +8,7 @@ const LightMode = (): JSX.Element => {
       viewBox="0 0 27 27"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="animeLeft"
     >
       <g clipPath="url(#clip0_1_28)">
         <path
