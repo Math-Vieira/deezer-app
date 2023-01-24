2ba07abeb25c3ec4306e7e35bf2e83de0299be93
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 18:29:34 -0300

    style: change z-index

--- src/pages/NotFound/style.ts
+++ src/pages/NotFound/style.ts
@@ -15,7 +15,7 @@ export const Container = styled.div`
   width: 100%;
   position: fixed;
   top: 0;
-  z-index: 2;
+  z-index: 10;
   display: flex;
   align-items: center;
   justify-content: center;
