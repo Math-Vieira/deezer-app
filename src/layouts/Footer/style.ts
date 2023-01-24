1e6b9c0b03b8eb43842eff4f6926c3108e8a1cf6
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 05:15:34 -0300

    style: create footer style

--- src/layouts/Footer/style.ts
+++ src/layouts/Footer/style.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    background-color: red;
+export const Container = styled.footer`
+  background-color: ${({ theme }) => theme.secondaryColor};
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 20px;
+  color: ${({ theme }) => theme.textColor.primaryColor};
 `
