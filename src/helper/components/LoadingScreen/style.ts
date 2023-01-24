7ef75fbbe6611e46c3c03f52dd5800769d3a7b74
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 03:14:13 -0300

    style: create styled components

--- src/helper/components/LoadingScreen/style.ts
+++ src/helper/components/LoadingScreen/style.ts
@@ -1,5 +1,25 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
 
 export const Container = styled.div`
-    background-color: red;
+  background-color: ${({ theme }) => theme.mainColor};
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+`
+export const InnerContent = styled.div`
+  background-color: ${({ theme }) => theme.secondaryColor};
+  animation: ${fadeIn} 0.2s linear alternate infinite;
+  width: 100%;
+  height: 100%;
 `
