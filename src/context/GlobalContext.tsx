0d5d94ce834618884ac7dd0ffab313ee6d57ec22
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 14:09:02 -0300

    feat: create toggleTheme function

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { ThemeContract, darkTheme } from '../themes/themes'
+import { ThemeContract, darkTheme, lightTheme } from '../themes/themes'
 import { Props, GlobalContextInterface } from './model'
 import GlobalStyle from '../themes/GlobalStyle'
 
@@ -8,18 +8,27 @@ const initialContext: GlobalContextInterface = {
   favoriteTracks: []
 }
 
-export const GlobalContext = createContext<GlobalContextInterface>(initialContext)
+export const GlobalContext =
+  createContext<GlobalContextInterface>(initialContext)
 
 const GlobalContextProvider = ({ children }: Props): JSX.Element => {
   const [favoriteTracks, setFavoriteTracks] = useState<any[]>([])
   const [theme, setTheme] = useState<ThemeContract>(darkTheme)
+  const toggleTheme = (): void => {
+    if (theme.themeId === 1) {
+      setTheme(lightTheme)
+    } else {
+      setTheme(darkTheme)
+    }
+  }
 
   return (
     <GlobalContext.Provider
       value={{
         favoriteTracks,
         setFavoriteTracks,
-        setTheme
+        toggleTheme,
+        themeId: theme.themeId
       }}
     >
       <GlobalStyle />
