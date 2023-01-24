a1f3c6723caf00777c4cffd9fa7e9c2af734c25d
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 04:56:47 -0300

    style: create notFund styled components

--- src/pages/NotFound/style.ts
+++ src/pages/NotFound/style.ts
@@ -1,5 +1,30 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
 
 export const Container = styled.div`
-    background-color: red;
+  background-color: ${({ theme }) => theme.mainColor};
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  & a {
+    color: ${({ theme }) => theme.textColor.primaryColor};
+  }
+`
+export const InnerContent = styled.h1`
+  font-size: 30px;
+  color: ${({ theme }) => theme.textColor.primaryColor};
 `
