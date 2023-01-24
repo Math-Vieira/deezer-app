72284986c0ab42b2107291803cd652f0d6ce08e2
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 05:16:07 -0300

    refactor: change footer text

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as S from './style'
 
 const Footer = (): JSX.Element => {
-  return <S.Container>Footer</S.Container>
+  return <S.Container>Deezer app - Matheus Vieira - 2023</S.Container>
 }
 
 export default Footer
