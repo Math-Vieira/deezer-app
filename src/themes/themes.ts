7912889e0335a4f2e206e63ab0e8d9c5046cc49a
math-vieira <matheusvieira9153@gmail.com>
2023-01-24 14:04:32 -0300

    style: create lightMode theme

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -1,4 +1,5 @@
 export interface ThemeContract {
+  themeId: number
   mainColor: string
   secondaryColor: string
   tertiaryColor: string
@@ -7,10 +8,12 @@ export interface ThemeContract {
   textColor: {
     primaryColor: string
     secondaryColor: string
+    tertiaryColor: string
   }
 }
 
 export const darkTheme: ThemeContract = {
+  themeId: 1,
   mainColor: '#0B0B0B',
   secondaryColor: '#272727',
   tertiaryColor: '#747474',
@@ -18,7 +21,21 @@ export const darkTheme: ThemeContract = {
   headerBoxShadow: '0px 2px 2px rgba(255, 255, 255, 0.25)',
   textColor: {
     primaryColor: '#FFFFFF',
-    secondaryColor: '#929292'
+    secondaryColor: '#929292',
+    tertiaryColor: '#FFFFFF'
   }
 }
-// export const lightTheme = {}
+
+export const lightTheme: ThemeContract = {
+  themeId: 2,
+  mainColor: '#FFFFFF',
+  secondaryColor: '#D7D7D7',
+  tertiaryColor: '#747474',
+  headerBackground: 'rgba(255, 255, 255, 0.5)',
+  headerBoxShadow: '0px 2px 2px rgba(0, 0, 0, 0.25)',
+  textColor: {
+    primaryColor: '#000000',
+    secondaryColor: '#929292',
+    tertiaryColor: '#FFFFFF'
+  }
+}
